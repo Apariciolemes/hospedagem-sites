1f474ca2db8e2756c411bdcee2a64fd7a8d917a1
Aparicio Lemes Neto <neto.lemes@devwarehouse.tech>
2023-04-17 23:35:20 -0300

    feat: adicionado action de logout

--- src/common/components/BaseHeader.vue
+++ src/common/components/BaseHeader.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="base-header">
     <img class="base-header__image" src="@/common/assets/logo.png" alt="logo" />
-    <BaseAvatar label="J" />
+    <div class="base-header__avatar">
+      <BaseAvatar label="J" @click="showActionLogout = !showActionLogout" />
+      <div
+        v-show="showActionLogout"
+        class="base-header__avatar__action"
+        @click="logout"
+      >
+        Sair
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import BaseAvatar from "./BaseAvatar.vue";
+
+const showActionLogout = ref(false);
+
+function logout() {
+  localStorage.clear();
+  window.location.reload();
+}
 </script>
 
 <style lang="scss">
@@ -18,6 +35,21 @@ import BaseAvatar from "./BaseAvatar.vue";
 
   &__image {
     width: 176px;
+  }
+
+  &__avatar {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    cursor: pointer;
+
+    &__action {
+      background: #ffffff;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-top: 4px;
+      padding: 12px 40px;
+    }
   }
 }
 </style>
